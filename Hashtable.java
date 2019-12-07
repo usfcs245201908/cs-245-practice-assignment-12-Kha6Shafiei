455e415721cc5e3cbf38e5b7873759d7439e79ee
Khash Shafiei <47541695+Kha6Shafiei@users.noreply.github.com>
2019-12-06 17:05:13 -0800

    Add files via upload

--- Hashtable.java
+++ Hashtable.java
@@ -1,140 +1,93 @@
-import java.util.*;
-
 public class Hashtable {
 
-    int size;
-    ArrayList<HashNode>bucket;  
-    int bucketnum;
 
-    private class HashNode
-    {
+    int sizeOfHashTable;
+    Node[] elements;
+
+    Hashtable(){
+        this.sizeOfHashTable = 1000;
+        this.elements = new Node[1000];
+    }
+
+    Hashtable(int size){
+        this.sizeOfHashTable = size;
+        this.elements = new Node[size];
+    }
+
+
+    private class Node {
         String key;
         String value;
-        HashNode next;
+        Node next;
 
-        public HashNode(String key, String value)
-        {
-            this.key=key;
-            this.value=value;
-            next=null;
+        public Node(String key, String value) {
+            this.key = key;
+            this.value = value;
+            this.next = null;
         }
     }
 
-    public Hashtable(){
-        bucket= new ArrayList<HashNode>();
-        bucketnum=2028;
-        size=0;
 
-        for (int i=0; i < bucketnum; i++){
-            bucket.add(null);
+    boolean containsKey(String key){
+        int hash = hash(key);
+        if(elements[hash] != null) {
+            return true;
         }
-    
 
+
+        return false;
     }
 
 
-    public void put (String key, String value){
-
-        int index = getHash(key);
-        HashNode head = bucket.get(index);
-
-        while (head != null)
-        {
-            if (head.key.equals(key))
-            {
-                head.value = value;
-                return;
+    String get(String key){
+        int hash = hash(key);
+        if(elements[hash] != null) {
+            Node temp = elements[hash];
+            while (temp != null) {
+                if (temp.key.equals(key)) {
+                    return temp.value;
+                }
+                temp = temp.next;
             }
-            head = head.next;
-        }
-
-        size++;
-        head = bucket.get(index);
-        HashNode newNode = new HashNode(key, value);
-        newNode.next=head;
-        bucket.set(index, newNode);
-
-        if ((1.0*size)/bucketnum >= 0.5)
-        {
-            ArrayList<HashNode> temp = bucket;
-            bucketnum = 2 * bucketnum; 
-            bucket = new ArrayList<HashNode>(bucketnum); 
-            size = 0; 
-
-            for (int i = 0; i < bucketnum; i++) {
-                bucket.add(null); 
-            }
-  
-            for (HashNode headReplace : temp) 
-            { 
-                while (headReplace != null) 
-                { 
-                    put(headReplace.key, headReplace.value); 
-                    headReplace = headReplace.next; 
-                } 
-            } 
-        }
-    }
-
-    
-    
-    public boolean containsKey(String key) {
-        
-        if (get(key)==null){
-            return false;
-        }
-        return true;
-    }
-
-
-  
-    public String get(String key){
-
-        int index = getHash(key);
-        HashNode head = bucket.get(index);
-
-        while(head!=null)
-        {
-            if((head.key).equals(key))
-            {
-                return head.value;
-            }
-            head=head.next;
         }
 
         return null;
     }
 
+    void put(String key, String value){
+        Node hashElement = new Node(key, value);
+        int hash = hash(key);
+        if(elements[hash] == null) {
+            elements[hash] = hashElement;
+        } else {
+            hashElement.next = elements[hash];
+            elements[hash] = hashElement;
+        }
+    }
 
-    private int getHash(String key){
-        return (Math.abs(key.hashCode()) % bucketnum);
+    String remove(String key){
+        int hash = hash(key);
+        if(elements[hash] != null) {
+            Node temp = elements[hash];
+            while (temp != null) {
+                if (temp.key.equals(key)) {
+                    elements[hash] = null;
+                    return temp.value;
+                }
+                temp = temp.next;
+            }
+
+
+        }
+        return null;
     }
 
 
-    public String remove(String key) throws Exception { 
 
-        int index = getHash(key);
-        HashNode head = bucket.get(index);
 
-        HashNode prev = null;
-        while (head != null && head.key.equals(key)==false)
-        {
-            prev = head;
-            head = head.next;
-        }
-
-        if (head == null){
-            throw new Exception();
-        }
-
-        size--;
-
-        if (prev != null){
-            prev.next = head.next;
-        }
-        else{
-            bucket.set(index, head.next);
-        }
-        return head.value;
+    private int hash(String key) {
+        int hashCode = Math.abs(key.hashCode());
+        return hashCode % sizeOfHashTable;
     }
+
 }
